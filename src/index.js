f8e5ec10970b2c9c17a1cb482e5898a82cb2e560
realisticattorney <g.axl.aquila@gmail.com>
2021-04-18 02:23:02 -0300

    Add search form query selector and event listener

--- src/index.js
+++ src/index.js
@@ -1,34 +1,27 @@
-// const importWeatherAPI = require ('./openWeatherAPI.js')
-// console.log(importWeatherAPI.weatherFetch)
+import asyncWeather from './APIfetcher.js'
 
-// fetch("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=1bc3162ef9f1c153a0e6581ca59c403f", {
-//   mode: 'cors'
-// }).then(response => {
-//   return response.json()
-// }).catch(e => {
-//   console.log(e)
-// }).finally(() => {
-//   console.log('finally')
-// })
+window.onload = () => {
+  lol('London')
+}
 
 
-async function weatherFetch(location = "London,uk") {
-  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=1bc3162ef9f1c153a0e6581ca59c403f`, {
-    mode: 'cors'
-  }).then(response => response.json()).catch(e => console.log(e))
-  return response
+
+const article = document.querySelector('#data')
+
+const searchForm = document.querySelector('#search-bar')
+
+searchForm.addEventListener('submit', selectQuery)
+
+function selectQuery(e) {
+  e.preventDefault()
+  if(searchForm[0].value == '') {return}
+  const city = e.target[0].value
+    while (article.firstChild) {
+    article.removeChild(article.firstChild);}
+    lol(city)
+  searchForm[0].value = ""
   }
-
-  weatherFetch()
-
-
-const img = document.querySelector('img');
+  
 
 
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=M2wtHygZrXHfXY1EehSvpMJI20mB7Bqp&s=cats', {mode: 'cors'})
-.then(function(response) {
-  return response.json();
-})
-.then(function(response) {
-  img.src = response.data.images.original.url;
-});
+
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,41 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  experiments: {
+    topLevelAwait: true,
+  },
   watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.(html)$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10 * 1024,
+          name: 'img/[hash].[ext]',
+        },
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-url-loader',
+        options: {
+          limit: 10 * 1024,
+          noquotes: true,
+        },
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        loader: 'image-webpack-loader',
+        enforce: 'pre',
+        options: {
+          bypassOnDebug: true,
+          disable: true,
+        },
+      },
+    ],
+  },
 };
