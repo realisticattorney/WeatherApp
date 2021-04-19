ea7a1e184a4ec26cf813cd91df55d566d86ffde9
realisticattorney <g.axl.aquila@gmail.com>
2021-04-19 04:09:08 -0300

    Remove try-catch

--- src/APIfetcher.js
+++ src/APIfetcher.js
@@ -1,22 +1,18 @@
 const asyncWeather = async (city = 'London') => {
-  try {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=1bc3162ef9f1c153a0e6581ca59c403f`, {
-      mode: 'cors',
-    });
-    const data = await response.json();
-    return {
-      humidity: data.main.humidity,
-      temp_max: data.main.temp_max,
-      temp_min: data.main.temp_min,
-      temp_celcius: data.main.temp,
-      city: data.name,
-      description: data.weather[0].description,
-      short_description: data.weather[0].main,
-      speed: data.wind.speed,
-      country: data.sys.country,
-    };
-  } catch (error) {
-  }
+  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=1bc3162ef9f1c153a0e6581ca59c403f`, {
+    mode: 'cors',
+  });
+  const data = await response.json();
+  return {
+    humidity: data.main.humidity,
+    temp_max: data.main.temp_max,
+    temp_min: data.main.temp_min,
+    temp_celcius: data.main.temp,
+    city: data.name,
+    description: data.weather[0].description,
+    short_description: data.weather[0].main,
+    speed: data.wind.speed,
+    country: data.sys.country,
+  };
 };
-
 export default asyncWeather;
