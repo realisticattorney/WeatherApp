4196880f2a5c5cb80e0d3caf850920383d7ca3b9
realisticattorney <g.axl.aquila@gmail.com>
2021-04-18 21:30:33 -0300

    Add convertion function between Celcius units and Fahrenheit units

--- src/APIfetcher.js
+++ src/APIfetcher.js
@@ -1,3 +1,4 @@
+
 export default async function asyncWeather(city = 'London') {
   const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=1bc3162ef9f1c153a0e6581ca59c403f`)
   response
@@ -9,7 +10,7 @@ export default async function asyncWeather(city = 'London') {
 humidity: data.main.humidity,
 temp_max:data.main.temp_max,
 temp_min:data.main.temp_min,
-temp_fah:data.main.temp,
+temp_celcius:data.main.temp,
 city:data.name,
 description:data.weather[0].description,
 short_description:data.weather[0].main,
--- src/index.js
+++ src/index.js
@@ -1,40 +1,62 @@
 import "bootswatch/dist/minty/bootstrap.min.css"
 import asyncWeather from './APIfetcher.js'
+import gif from "./GIFfetcher.js"
 
 window.onload = () => {
-  lol('London')
+  weatherDOM('London')
 }
 
-
-
-const article = document.querySelector('#data')
-
 const searchForm = document.querySelector('#search-bar')
-
 searchForm.addEventListener('submit', selectQuery)
 
 function selectQuery(e) {
   e.preventDefault()
   if(searchForm[0].value == '') {return}
   const city = e.target[0].value
-    lol(city)
+    weatherDOM(city)
   searchForm[0].value = ""
   }
   
   
-function lol(e) {
+function weatherDOM(e) {
     const input = e
     const promise = asyncWeather(`${input}`)
     const result = promise.then(function(result){
       for(let property in result) {
         const query = `#${property}`
         let prop = document.querySelector(query)  
-        console.log(prop)
     prop.innerText = `${result[property]}`
-  //  prop.setAttribute('class', result[property] )
-}
+    if(property === 'temp_max' || property === 'temp_min' || property === 'temp_celcius') {
+      prop.innerText = `${Math.round(result[property] - 273.15)} C`
+      prop.celcius = `${Math.round(result[property] - 273.15)}`
+    }
+    if(property === 'short_description') {
+      prop.style.display = "none";
+      gif(`${result[property]}`)
+    }}
 })
-  }
+}
 
+const celcius_button = document.querySelector('#converter')
+celcius_button.addEventListener('click', converter)
+
+function converter(e) {
+  const temp = document.querySelector('#temp_celcius')
+  const temp_max = document.querySelector('#temp_max')
+  const temp_min = document.querySelector('#temp_min')
+  if(e.path[0].className == 'btn btn-secondary') {
+    temp.innerHTML = `${(parseInt(temp.innerHTML) *(9/5)) + 32} F`
+    temp_max.innerHTML = `${(parseInt(temp_max.innerHTML) *(9/5)) + 32} F`
+    temp_min.innerHTML = `${(parseInt(temp_min.innerHTML) *(9/5)) + 32} F`
+    e.path[0].className = 'btn btn-primary'
+    e.path[0].innerText = 'Convert to Celcius'
+  } else {
+  temp.innerHTML = `${temp.celcius} C`
+  temp_max.innerHTML = `${temp_max.celcius} C`
+  temp_min.innerHTML = `${temp_min.celcius} C`
+  e.path[0].className = 'btn btn-secondary'
+  e.path[0].innerText = 'Convert to Fahrenheit'
+}
+}
 
 
